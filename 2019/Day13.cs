34a48c462bc054816719b947d4f09cb79383e7f1
mschamp <34092708+mschamp@users.noreply.github.com>
2020-12-07 20:25:29 +0100

    2019_Day13 solved

---
+++ 2019/Day13.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace _2019
+{
+    public class Day13 : General.IAoC
+    {
+        public string SolvePart1(string input = null)
+        {
+            IntcodeComputer computer = new IntcodeComputer();
+            computer.loadProgram(input);
+            computer.ExecuteProgram();
+            List<long> output = computer.ReadOutputs();
+            List<Tuple<long, long, long>> screen = new List<Tuple<long, long, long>>();
+            for (int i = 0; i < output.Count; i+=3)
+            {
+                screen.Add(new Tuple<long, long, long>(output[i], output[i + 1], output[i + 2]));
+            }
+            return "" + screen.Count(x => x.Item3 == 2);
+        }
+
+        public string SolvePart2(string input = null)
+        {
+            IntcodeComputer computer = new IntcodeComputer();
+            computer.loadProgram(input);
+            computer.SetMemoryContent(0, 2);
+            computer.ExecuteProgram();
+            List<long> output;
+            long xBall =0;
+            long xPaddle = 0;
+            Dictionary<General.clsPoint, long> screen = new Dictionary<General.clsPoint, long>();
+
+            while (computer.WaitingForInput)
+            {
+                
+                output = computer.ReadOutputs();
+                for (int i = 0; i < output.Count; i += 3)
+                {
+                    screen[new General.clsPoint((int)output[i], (int)output[i + 1])]= output[i + 2];
+                    if (output[i + 2]==3)
+                    {
+                        xPaddle = output[i];
+                    }
+                    else if (output[i + 2] == 4)
+                    {
+                        xBall = output[i];
+                    }
+                }
+                computer.ClearOutputs();
+
+                printScreen(screen);
+                computer.InputValue(DetermingInput(xBall, xPaddle));
+                computer.ExecuteProgram();
+            }
+
+            output = computer.ReadOutputs();
+            for (int i = 0; i < output.Count; i += 3)
+            {
+                screen[new General.clsPoint((int)output[i], (int)output[i + 1])] = output[i + 2];
+            }
+
+            return "" + screen[new General.clsPoint(-1, 0)];
+        }
+
+        private void printScreen(Dictionary<General.clsPoint, long> screen)
+        {
+            int row = -1;
+            string text = "";
+            foreach (KeyValuePair< General.clsPoint, long> item in screen.OrderBy(x => x.Key))
+            {
+                if (item.Key.Y>row)
+                {
+                    text += Environment.NewLine;
+                    row = item.Key.Y;
+                }
+                switch (item.Value)
+                {
+                    case 0:
+                        text += (" ");
+                        break;
+                    case 1:
+                        text += ("B");
+                        break;
+                    case 2:
+                        text += (".");
+                        break;
+                    case 3:
+                        text += ("_");
+                        break;
+                    case 4:
+                        text += ("O");
+                        break;
+                    default:
+                        break;
+                } 
+            }
+            //Console.Clear();
+            //Console.WriteLine(text);
+        }
+
+        private int DetermingInput(long ball, long paddle)
+        {
+            long delta = ball - paddle;
+            return (paddle > ball) ? -1 : (paddle < ball) ? 1 : 0;
+
+        }
+
+        public void Tests()
+        {
+            
+        }
+    }
+}
--- General/Program.cs
+++ General/Program.cs
@@ -15,14 +15,15 @@ namespace General
         private static void Solve2020()
         {
             List<IAoC> days = new List<IAoC> { new _2020.Day1(), new _2020.Day2(), new _2020.Day3(), new _2020.Day4(), new _2020.Day5(),
-            new _2020.Day6(),new _2020.Day7()};
+            new _2020.Day6(),new _2020.Day7(),new _2020.Day8()};
             string[] inputs = new[] { _2020.inputs.D1P1, _2020.inputs.D1P1,
                 _2020.inputs.D2P1, _2020.inputs.D2P1,
             _2020.inputs.D3P1, _2020.inputs.D3P1,
             _2020.inputs.D4P1, _2020.inputs.D4P1,
             _2020.inputs.D5P1, _2020.inputs.D5P1,
             _2020.inputs.D6P1, _2020.inputs.D6P1,
-            _2020.inputs.D7P1, _2020.inputs.D7P1};
+            _2020.inputs.D7P1, _2020.inputs.D7P1,
+             _2020.inputs.D8P1, _2020.inputs.D8P1};
             Stopwatch watch = new Stopwatch();
 
             for (int i = 0; i < days.Count; i++)
@@ -44,7 +45,7 @@ namespace General
         {
             List<IAoC> days = new List<IAoC> { new _2019.Day1(), new _2019.Day2(), new _2019.Day3(), new _2019.Day4(), new _2019.Day5(),
             new _2019.Day6(), new _2019.Day7(), new _2019.Day8(), new _2019.Day9(), new _2019.Day10(),
-            new _2019.Day11(), new _2019.Day12()};
+            new _2019.Day11(), new _2019.Day12(), new _2019.Day13()};
             string[] inputs = new[] { _2019.inputs.D1P1, _2019.inputs.D1P1,
                 _2019.inputs.D2P1, _2019.inputs.D2P2,
             _2019.inputs.D3P1, _2019.inputs.D3P1,
@@ -56,7 +57,8 @@ namespace General
             _2019.inputs.D9P1, _2019.inputs.D9P1,
             _2019.inputs.D10P1, _2019.inputs.D10P1,
             _2019.inputs.D11P1, _2019.inputs.D11P1,
-            _2019.inputs.D12P1, _2019.inputs.D12P1};
+            _2019.inputs.D12P1, _2019.inputs.D12P1,
+            _2019.inputs.D13P1, _2019.inputs.D13P1};
             Stopwatch watch = new Stopwatch();
 
             for (int i = 0; i < days.Count; i++)
