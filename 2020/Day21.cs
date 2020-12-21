1ae7f891606de797da5b354e6d0a846d45f46659
mschamp <34092708+mschamp@users.noreply.github.com>
2020-12-21 09:03:36 +0100

    2020 Day 21 Solved

---
+++ 2020/Day21.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _2020
+{
+    public class Day21 : General.IAoC
+    {
+        public string SolvePart1(string input = null)
+        {
+            List<Food> foods = new List<Food>();
+            foreach (string item in input.Split(Environment.NewLine))
+            {
+                foods.Add(new Food(item));
+            }
+
+            Dictionary<string, string> AllergenIngredient = new Dictionary<string, string>();
+            HashSet<string> ingredients = new HashSet<string>();
+            foreach (Food food in foods)
+            {
+                foreach (string allergen in food.Allergens)
+                {
+                    AllergenIngredient[allergen] = null;
+                }
+                foreach (string ingred in food.Ingredients)
+                {
+                    ingredients.Add(ingred);
+                }
+                
+            }
+
+            List<String> Allergens = AllergenIngredient.Keys.ToList();
+            while (AllergenIngredient.Values.Contains(null))
+            {
+                foreach (string Allerg in Allergens)
+                {
+                    if (AllergenIngredient[Allerg]==null)
+                    {
+                        List<string> options = foods.Where(x => x.Allergens.Contains(Allerg)).First().Ingredients;
+                        foreach (var Assigned in AllergenIngredient.Values)
+                        {
+                            options.Remove(Assigned);
+                        }
+                        
+                        foreach (var recept in foods.Where(x => x.Allergens.Contains(Allerg)))
+                        {
+                            options = options.Intersect(recept.Ingredients).ToList();
+                        }
+                        if (options.Count==1)
+                        {
+                            AllergenIngredient[Allerg] = options[0];
+                            ingredients.Remove(options[0]);
+                        }
+                    }
+
+                }
+            }
+
+            int Counter = 0;
+            foreach (Food food in foods)
+            {
+                foreach (string ingred in food.Ingredients)
+                {
+                    if (ingredients.Contains(ingred))
+                    {
+                        Counter++;
+                    }
+                }
+            }
+
+
+
+            return "" + Counter;
+        }
+
+        public string SolvePart2(string input = null)
+        {
+            List<Food> foods = new List<Food>();
+            foreach (string item in input.Split(Environment.NewLine))
+            {
+                foods.Add(new Food(item));
+            }
+
+            Dictionary<string, string> AllergenIngredient = new Dictionary<string, string>();
+            HashSet<string> ingredients = new HashSet<string>();
+            foreach (Food food in foods)
+            {
+                foreach (string allergen in food.Allergens)
+                {
+                    AllergenIngredient[allergen] = null;
+                }
+                foreach (string ingred in food.Ingredients)
+                {
+                    ingredients.Add(ingred);
+                }
+
+            }
+
+            List<String> Allergens = AllergenIngredient.Keys.ToList();
+            while (AllergenIngredient.Values.Contains(null))
+            {
+                foreach (string Allerg in Allergens)
+                {
+                    if (AllergenIngredient[Allerg] == null)
+                    {
+                        List<string> options = foods.Where(x => x.Allergens.Contains(Allerg)).First().Ingredients;
+                        foreach (var Assigned in AllergenIngredient.Values)
+                        {
+                            options.Remove(Assigned);
+                        }
+
+                        foreach (var recept in foods.Where(x => x.Allergens.Contains(Allerg)))
+                        {
+                            options = options.Intersect(recept.Ingredients).ToList();
+                        }
+                        if (options.Count == 1)
+                        {
+                            AllergenIngredient[Allerg] = options[0];
+                            ingredients.Remove(options[0]);
+                        }
+                    }
+
+                }
+            }
+
+            List<string> Ordered = new List<string>();
+            foreach (var item in Allergens.OrderBy(x =>x))
+            {
+                Ordered.Add(AllergenIngredient[item]);
+            }
+
+            return string.Join(",", Ordered);
+        }
+
+        public void Tests()
+        {
+            Debug.Assert(SolvePart1(@"mxmxvkd kfcds sqjhc nhms (contains dairy, fish)
+trh fvjkl sbzzf mxmxvkd (contains dairy)
+sqjhc fvjkl (contains soy)
+sqjhc mxmxvkd sbzzf (contains fish)") == "5");
+
+            Debug.Assert(SolvePart2(@"mxmxvkd kfcds sqjhc nhms (contains dairy, fish)
+trh fvjkl sbzzf mxmxvkd (contains dairy)
+sqjhc fvjkl (contains soy)
+sqjhc mxmxvkd sbzzf (contains fish)") == "mxmxvkd,sqjhc,fvjkl");
+
+            
+        }
+
+        
+    }
+
+    public class Food
+    {
+        public Food(string Input)
+        {
+            Regex rgx = new Regex(@"((?:\w+\s)+)\(contains\s((?:\w+(?:,\s)?)+)\)");
+            Match mtch = rgx.Match(Input);
+            Ingredients = mtch.Groups[1].Value.Split(" ",StringSplitOptions.RemoveEmptyEntries).ToList();
+            Allergens = mtch.Groups[2].Value.Split(", ").ToList();
+        }
+
+        public List<String> Ingredients { get; set; }
+        public List<String> Allergens { get; set; }
+    }
+}
--- General/Program.cs
+++ General/Program.cs
@@ -8,7 +8,11 @@ namespace General
     {
         static void Main(string[] args)
         {
-           //Solve2019();
+            Solve2015();
+            Solve2016();
+            Solve2017();
+            Solve2018();
+            //Solve2019();
             Solve2020();
         }
 
@@ -17,7 +21,8 @@ namespace General
             List<IAoC> days = new List<IAoC> { new _2020.Day1(), new _2020.Day2(), new _2020.Day3(), new _2020.Day4(), new _2020.Day5(),
             new _2020.Day6(),new _2020.Day7(),new _2020.Day8(),new _2020.Day9(),new _2020.Day10(),
             new _2020.Day11(),new _2020.Day12(),new _2020.Day13(),new _2020.Day14(),new _2020.Day15(),
-            new _2020.Day16(),new _2020.Day17(),new _2020.Day18(),new _2020.Day19(), new _2020.Day20()};
+            new _2020.Day16(),new _2020.Day17(),new _2020.Day18(),new _2020.Day19(), new _2020.Day20(),
+            new _2020.Day21()};
             string[] inputs = new[] { _2020.inputs.D1P1, _2020.inputs.D1P1,
             _2020.inputs.D2P1, _2020.inputs.D2P1,
             _2020.inputs.D3P1, _2020.inputs.D3P1,
@@ -37,20 +42,10 @@ namespace General
             _2020.inputs.D17P1, _2020.inputs.D17P1,
             _2020.inputs.D18P1, _2020.inputs.D18P1,
             _2020.inputs.D19P1, _2020.inputs.D19P1,
-            _2020.inputs.D20P1, _2020.inputs.D20P1};
-            Stopwatch watch = new Stopwatch();
+            _2020.inputs.D20P2, _2020.inputs.D20P2,
+            _2020.inputs.D21P1, _2020.inputs.D21P1};
 
-            for (int i = 0; i < days.Count; i++)
-            {
-                days[i].Tests();
-                Console.WriteLine($"DAY{i + 1}");
-                watch.Start();
-                Console.WriteLine(days[i].SolvePart1(inputs[2 * i]) + " " + watch.Elapsed);
-                watch.Restart();
-                Console.WriteLine(days[i].SolvePart2(inputs[2 * i + 1]) + " " + watch.Elapsed);
-                watch.Stop();
-                Console.WriteLine("__________________________________");
-            }
+            Solve(days, inputs);
 
         }
 
@@ -75,20 +70,58 @@ namespace General
             _2019.inputs.D13P1, _2019.inputs.D13P1,
             _2019.inputs.D14P1, _2019.inputs.D14P1,
             _2019.inputs.D15P1, _2019.inputs.D15P1};
+
+            Solve(days, inputs);
+
+        }
+
+        private static void Solve2018()
+        {
+            List<IAoC> days = new List<IAoC> { };
+            string[] inputs = new string[] {};
+
+            Solve(days, inputs);
+        }
+
+        private static void Solve2017()
+        {
+            List<IAoC> days = new List<IAoC> { };
+            string[] inputs = new string[] { };
+
+            Solve(days, inputs);
+        }
+
+        private static void Solve2016()
+        {
+            List<IAoC> days = new List<IAoC> { };
+            string[] inputs = new string[] { };
+
+            Solve(days, inputs);
+        }
+
+        private static void Solve2015()
+        {
+            List<IAoC> days = new List<IAoC> { };
+            string[] inputs = new string[] { };
+
+            Solve(days, inputs);
+        }
+
+        private static void Solve(List<IAoC> days, string[] inputs)
+        {
             Stopwatch watch = new Stopwatch();
 
             for (int i = 0; i < days.Count; i++)
             {
                 days[i].Tests();
-                Console.WriteLine($"DAY{i+1}");
+                Console.WriteLine($"DAY{i + 1}");
                 watch.Start();
-                Console.WriteLine(days[i].SolvePart1(inputs[2*i]) + " " + watch.Elapsed);
+                Console.WriteLine(days[i].SolvePart1(inputs[2 * i]) + " " + watch.Elapsed);
                 watch.Restart();
-                Console.WriteLine(days[i].SolvePart2(inputs[2 * i+1]) + " " + watch.Elapsed);
+                Console.WriteLine(days[i].SolvePart2(inputs[2 * i + 1]) + " " + watch.Elapsed);
                 watch.Stop();
                 Console.WriteLine("__________________________________");
             }
-
         }
 
     }
