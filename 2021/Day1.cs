d2ec5937d06b5112f39697edd99c034e29c7b676
mschamp <34092708+mschamp@users.noreply.github.com>
2021-12-01 07:28:16 +0100

    Solve 2021 day1

--- 2021/Day1.cs
+++ 2021/Day1.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 
 namespace _2021
 {
@@ -7,17 +10,57 @@ namespace _2021
     {
         public string SolvePart1(string input = null)
         {
-            throw new NotImplementedException();
+            List<int> depths = input.Split(Environment.NewLine).Select(x => int.Parse(x)).ToList();
+
+            int Previous = depths.First();
+            int larger = 0;
+
+            for (int i = 1; i < depths.Count(); i++)
+            {
+                if (depths[i] > Previous) larger++;
+                Previous = depths[i];
+            }
+            return larger.ToString();
         }
 
         public string SolvePart2(string input = null)
         {
-            throw new NotImplementedException();
+            List<int> depths = input.Split(Environment.NewLine).Select(x => int.Parse(x)).ToList();
+
+            int Previous = depths[0]+ depths[1]+ depths[2];
+            int larger = 0;
+
+            for (int i = 3; i < depths.Count(); i++)
+            {
+                int moving = depths[i] + depths[i - 1] + depths[i - 2];
+                if (moving > Previous) larger++;
+                Previous = moving;
+            }
+            return larger.ToString();
         }
 
         public void Tests()
         {
-            throw new NotImplementedException();
+            Debug.Assert(SolvePart1(@"199
+200
+208
+210
+200
+207
+240
+269
+260
+263") == "7");
+            Debug.Assert(SolvePart2(@"199
+200
+208
+210
+200
+207
+240
+269
+260
+263") == "5");
         }
     }
 }
