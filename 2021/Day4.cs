72bb87ad6c84251e279ccf91351731be4bf447a8
mschamp <34092708+mschamp@users.noreply.github.com>
2021-12-04 09:48:02 +0100

    Day 4 cleaned

--- 2021/Day4.cs
+++ 2021/Day4.cs
@@ -4,17 +4,27 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using General.Extensions;
 
 namespace _2021
 {
-    public class Day4 : General.IAoC
+    public class Day4 : General.PuzzleWithObjectInput<(List<int>,List<Day4.Board>, Dictionary<int, Day4.BingoNumber>)>
     {
-        public string SolvePart1(string input = null)
+        public override (List<int>, List<Board>, Dictionary<int, BingoNumber>) CastToObject(string RawData)
         {
-            string[] parts = input.Split(Environment.NewLine + Environment.NewLine);
+            string[] parts = RawData.Split(Environment.NewLine + Environment.NewLine);
             List<int> Numbers = parts[0].Split(",").Select(x => int.Parse(x)).ToList();
             Dictionary<int, BingoNumber> dict = new Dictionary<int, BingoNumber>();
             var boards = parts.Skip(1).Select(x => new Board(x, ref dict)).ToList();
+
+            return (Numbers, boards, dict);
+        }
+
+        public override string SolvePart1((List<int> , List<Board>, Dictionary<int, BingoNumber>) input)
+        {
+            List<int> Numbers = input.Item1;
+            Dictionary<int, BingoNumber> dict = input.Item3;
+            List<Board> boards = input.Item2;
 
             int draw = -1;
             while (!boards.Any(x => x.bingo()))
@@ -25,18 +35,17 @@ namespace _2021
 
             Board winning = boards.Where(x => x.bingo()).First();
 
-            return (winning.SumUnmarked()*Numbers[draw]).ToString();
+            return (winning.SumUnmarked() * Numbers[draw]).ToString();
         }
 
-        public string SolvePart2(string input = null)
+        public override string SolvePart2((List<int>, List<Board>, Dictionary<int, BingoNumber>) input)
         {
-            string[] parts = input.Split(Environment.NewLine + Environment.NewLine);
-            List<int> Numbers = parts[0].Split(",").Select(x => int.Parse(x)).ToList();
-            Dictionary<int, BingoNumber> dict = new Dictionary<int, BingoNumber>();
-            var boards = parts.Skip(1).Select(x => new Board(x, ref dict)).ToList();
+            List<int> Numbers = input.Item1;
+            Dictionary<int, BingoNumber> dict = input.Item3;
+            List<Board> boards = input.Item2;
 
             int draw = -1;
-            while (boards.Count(x => !x.bingo())>1)
+            while (boards.Count(x => !x.bingo()) > 1)
             {
                 draw++;
                 dict[Numbers[draw]].Drawn = true;
@@ -53,7 +62,7 @@ namespace _2021
             return (losing.SumUnmarked() * Numbers[draw]).ToString();
         }
 
-        public void Tests()
+        public override void Tests()
         {
             Debug.Assert(SolvePart1(@"7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
 
@@ -133,18 +142,18 @@ namespace _2021
 
             public bool bingo()
             {
-                for (int i = 0; i < 5; i++)
+                for (int i = 0; i < numbers.GetLength(1); i++)
                 {
-                    if (getRow(numbers, i).All(x => x.Drawn))
+                    if (numbers.getRow(i).All(x => x.Drawn))
                     {
                         return true;
                     }
                     
                 }
 
-                for (int i = 0; i < 5; i++)
+                for (int i = 0; i < numbers.GetLength(0); i++)
                 {
-                    if (getColumn(numbers, i).All(x => x.Drawn))
+                    if (numbers.getColumn(i).All(x => x.Drawn))
                     {
                         return true;
                     }
@@ -153,30 +162,13 @@ namespace _2021
 
                 return false;
             }
-
-            private BingoNumber[] getRow(BingoNumber[,] matrix, int rowNumber)
-            {
-                return Enumerable.Range(0, matrix.GetLength(1))
-                .Select(x => matrix[rowNumber, x])
-                .ToArray();
-            }
-
-            private BingoNumber[] getColumn(BingoNumber[,] matrix, int columnNumber)
-            {
-                return Enumerable.Range(0, matrix.GetLength(0))
-                .Select(x => matrix[x, columnNumber])
-                .ToArray();
-            }
         
             public int SumUnmarked()
             {
                 int sum = 0;
                 for (int i = 0; i < numbers.GetLength(0); i++)
                 {
-                    for (int j = 0; j < numbers.GetLength(0); j++)
-                    {
-                        sum += numbers[i,j].Drawn?0:numbers[i,j].Number;
-                    }
+                    sum += numbers.getColumn(i).Where(x=> !x.Drawn).Sum(x => x.Number);
                 }
                 return sum;
             }
---
+++ Interfaces/Extensions/ArrayExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace General.Extensions
+{
+    public static class ArrayExtensions
+    {
+        public static T[] getRow<T>(this T[,] matrix, int rowNumber)
+        {
+            return Enumerable.Range(0, matrix.GetLength(1))
+            .Select(x => matrix[rowNumber, x])
+            .ToArray();
+        }
+
+        public static T[] getColumn<T>(this T[,] matrix, int columnNumber)
+        {
+            return Enumerable.Range(0, matrix.GetLength(0))
+            .Select(x => matrix[x, columnNumber])
+            .ToArray();
+        }
+    }
+}
